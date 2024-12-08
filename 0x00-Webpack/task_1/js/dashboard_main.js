f53105336f19a1511369653bc30c0d47c2c1d768
rakati <ouhaddounouredine@gmail.com>
2024-12-08 20:28:45 +0100

    copy

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,15 +1,7 @@
 const $ = require( 'jquery' );
 var _ = require('lodash');
 
-$('body').append('<p>ALX Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-
-$('body').append('<button>Click here to get started</button>');
-
-$('body').append('<p id=\'count\'></p>');
-$('body').append('<p>Copyright - ALX</p>');
-
-let count = 0
+let count = 0;
 
 function updateCounter() {
   // count number of times the button element has been clicked.
@@ -17,4 +9,16 @@ function updateCounter() {
   $('#count').text(`${count} clicks on the button`)
 };
 
-$('button').on('click', _.debounce(updateCounter, 500))
+const $button = $("<button>Click here to get started</button>").on(
+  "click",
+  _.debounce( updateCounter, 500, { leading: true, trailing: false })
+);
+
+
+$('body').append('<p>ALX Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+
+$('body').append($button);
+
+$('body').append('<p id=\'count\'></p>');
+$('body').append('<p>Copyright - ALX</p>');
